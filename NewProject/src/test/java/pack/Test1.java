70775c9ce71318e625243905c0d7b7076d8dcf76
malaganti <malaganti@AVINASH>
2021-07-26 19:06:29 +0530

    my project

---
+++ NewProject/src/test/java/pack/Test1.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Test1
+{
+
+	public static void main(String[] args) 
+	{
+		 int x=10;
+				 int y=120;
+				 int z=x+y;
+				 System.out.println("value of "+z);
+
+	}
+
+}
